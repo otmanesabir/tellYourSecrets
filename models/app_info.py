90ca0f9bbf39efe61093b36de6793bbf6e1254b0
Otmane Sabir <o.sabir@jacobs-university.de>
2020-12-29 17:39:22 +0100

    added bundleID as key

--- models/app_info.py
+++ models/app_info.py
@@ -1,5 +1,8 @@
+# uses the bundle ID as the file name and the main app identifier.
+
 class app_info:
-    def __init__(self, app_name, android_link, apple_link, last_saved_review):
+    def __init__(self, bundle_id, app_name, android_link, apple_link, last_saved_review):
+        self.bundle_id = bundle_id
         self.app_name = app_name
         self.android_link = android_link
         self.apple_link = apple_link
@@ -8,6 +11,7 @@ class app_info:
     def __repr__(self):
         return(
             f'app_info(\
+                bundle_id={self.bundle_id}, \
                 app_name={self.app_name}, \
                 android_link={self.android_link}, \
                 apple_link={self.apple_link}, \
