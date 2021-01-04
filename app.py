2e22875817c0d019a7bb98175624e853413e9400
Otmane Sabir <o.sabir@jacobs-university.de>
2021-01-04 14:50:39 +0100

    logic++

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask.helpers import make_response
 from crawler_tasks import search_app as sa
 from crawler_tasks import get_reviews as gr
+from models import app_info as ai
 import requests
 import logger
 
@@ -24,19 +25,20 @@ def receive_test():
 @app.route('/post', methods=['POST'])
 def get_request():
     if not request.is_json:
-        log.error("Did not receive JSON will not proceed")
-        return make_response('Bad Request', 404)
+        log.error("did not receive valid JSON... will not proceed")
+        return make_response('Bad Request', 400)
     data = request.get_json()
     if (data["search_type"] == "apps"):
         results = sa.delay(data["crawler"], data["keyword"], data["start_index"]).get()
         gr.delay(data["crawler"], results) # don't really need to wait for these results.
-        bundles = []
-        for result in results:
-            bundles.append(result["bundle_id"])
-        dictToSend = {'bundle_ids': bundles}
+        dictToSend = {'bundle_ids': [d['value'] for d in results]}
+        # TODO SEND TO THE WEB APP
         res = requests.post('http://localhost:5000/post/receive/apps', json=dictToSend)
         print('response from server:', res.status_code)
-        return make_response('Received !', 201) # response to your request.
+    elif (data["search_type"] == "reviews"):
+        apps = ai.app_info.load_from_bundle_id(data["bundle_ids"])
+        gr.delay(data["crawler"], apps)
+    return make_response('Succesfully Processed!', 201) # response to our request.
 
 if __name__ == "__main__":
     app.run()
