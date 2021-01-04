bc862067d0779d5d0332d313e3bee20b912be6f5
Otmane Sabir <o.sabir@jacobs-university.de>
2021-01-04 14:00:21 +0100

    refactor cralwer_logic

--- crawler_tasks.py
+++ crawler_tasks.py
@@ -9,11 +9,26 @@ ct = crawler_interface.crawler_types
 app = Celery('crawler_tasks', backend='rpc://', broker='pyamqp://', task_serializer='pickle', result_serializer='pickle', accept_content=['application/json'])
 
 @app.task
-def get_reviews(value):
-    apps, crawler_type = value
+def search_and_get_reviews(crawler_type, app_name, start_idx):
     res = []
+    if (crawler_type == ct.PLAY_STORE):
+        res = play_store.play_store_crawler().search_app_name(app_name, start_idx)
+    elif (crawler_type == ct.APP_STORE):
+        res = app_store.app_store_crawler().search_app_name(app_name, start_idx)
+    elif (crawler_type == ct.ALL):
+        res = play_store.play_store_crawler().search_app_name(app_name, start_idx)
+        res.append(app_store.app_store_crawler().search_app_name(app_name, start_idx))
+    return get_reviews(crawler_type, res)
+
+@app.task
+def get_reviews(crawler_type, apps):
+    res = []
+    if not type(apps) is list:
+        apps = [apps]
+        print("converted to list")
     for app_info in apps:
-        app_info = ai.app_info.from_dict(app_info)
+        if not type(app_info) is ai.app_info:
+            app_info = ai.app_info.from_dict(app_info)
         if (crawler_type == ct.PLAY_STORE):
             res.append(play_store.play_store_crawler().get_reviews(app_info))
         elif (crawler_type == ct.APP_STORE):
@@ -33,4 +48,4 @@ def search_app(crawler_type, app_name, start_idx):
     elif (crawler_type == ct.ALL):
         res = play_store.play_store_crawler().search_app_name(app_name, start_idx)
         res.append(app_store.app_store_crawler().search_app_name(app_name, start_idx))
-    return res, crawler_type
+    return res
